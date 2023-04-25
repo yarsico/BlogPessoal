932a4df3a38551e8dc6322f70d40c52209b3ef86
unknown <raylanes315@gmail.com>
2023-04-25 09:18:02 -0300

    Atualização

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,16 +1,21 @@
 package com.generation.blogpessoal.model;
 
+
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "tb_postagens")
@@ -30,6 +35,10 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
     public Long getId() {
         return this.id;
@@ -62,5 +71,13 @@ public class Postagem {
     public void setData(LocalDateTime data) {
         this.data = data;
     }
-  
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+    
 }
